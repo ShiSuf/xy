0ecca12685a917728076f1c985afbd20cfdc79af
ShiSuf <3286916335@qq.com>
2023-01-07 19:56:58 +0800

    01

---
+++ apps/xy.js
@@ -0,0 +1,33 @@
+import { segment } from "oicq";//导包部分
+import fetch from "node-fetch";
+import { Config} from '../components/index.js'
+
+const _path = process.cwd();
+
+export class example extends plugin{
+    constructor () {
+            super({
+                name:'丁真',
+                dsc:'坤坤',
+                event:'message',
+                priority: 4444,
+                rule:[
+                    {
+             /** 命令正则匹配 */
+            reg: '^((.*)鸡你太美(.*)|(.*)坤坤(.*)|(.*)小黑子(.*)|(.*)鲲鲲(.*)|(.*)鸽鸽(.*))$',
+            /** 执行方法 */
+            fnc: 'jntm'
+                    }
+                ]
+            })
+    }
+    async xy (e){
+        let msg = [
+            segment.at(e.user.id),
+            "欢迎使用xy插件",
+            segment.image('file:///${_path}/resources/01.png')
+        ]
+        e.reply(msg);
+        return true;
+    }
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+import fs from 'node:fs'
+import Ver from './components/Version.js'
+import chalk from 'chalk'
+
+const files = fs.readdirSync('./plugins/xy/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+logger.info('欢迎使用xy插件')
+
+files.forEach((file) => {
+    ret.push(import(`./apps/${file}`))
+})
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+    let name = files[i].replace('.js', '')
+
+    if (ret[i].status != 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        logger.error(ret[i].reason)
+        continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+export { apps }
